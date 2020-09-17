93044aea07feabc76d3cac5cc36b6c67a93a6746
Aleksey Stepanov <stepanovl87@gmail.com>
2020-09-17 20:13:14 +0300

    Content block its ok

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import ContentBlock from './components/ContentBlock/ContentBlock';
 import HeaderBlock from './components/HeaderBlocks/HeaderBlock'
 
 const App = () => {
+  const Cards = {
+    col: 6,
+  }
   return (
     <React.Fragment>
     <HeaderBlock title="Учите слова онлайн!"/>
+    <ContentBlock {...Cards}/>
     </React.Fragment>
   )
 }
